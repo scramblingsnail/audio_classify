27309a08e32155f352aab8ebaf65b41a0df66823
Yichen Zhao <njuzhaoyichen@gmail.com>
2024-06-12 17:59:46 +0800

    update files

--- src/data_preprocess/process_provided_data.py
+++ src/data_preprocess/process_provided_data.py
@@ -50,7 +50,7 @@ def board_preprocess(board_data_dir):
                     data = f.read(data_bytes_num)
                     int_data = int.from_bytes(data, byteorder='little', signed=True)
                     wav_data[0, i // data_bytes_num] = int_data / amplitude
-
+        
         wav_f_name = f_name.split('.')[0] + '.wav'
         wav_f_path = os.path.join(board_data_dir, wav_f_name)
         print('{} has been saved as {}'.format(f_name, wav_f_name))
@@ -58,5 +58,5 @@ def board_preprocess(board_data_dir):
 
 
 if __name__ == '__main__':
-    board_preprocess(r'D:\Datasets\competitions\xinyuan\data\board')
-    pc_preprocess(r'D:\python_works\audio_classify\data\pc')
+    board_preprocess(r'./data/board')
+    pc_preprocess(r'./data/pc')
