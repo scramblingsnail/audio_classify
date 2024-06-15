37b8167c72df752223ac3a0a103ddd56e37f11f6
zhisan <zaizheng@isrc.iscas.ac.cn>
2024-06-15 23:32:01 +0800

    modified main docs

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from src.pipeline import classify_pipeline, test_pipeline, debug
 
 if __name__ == '__main__':
 	r""" modify the 'config_flag' to your own configuration file name. """
-	# classify_pipeline(config_flag='config2')
+	classify_pipeline(config_flag='config2')
 	# test_pipeline(config_flag='config1')
-	debug(config_flag='config2')
+	# debug(config_flag='config2')
